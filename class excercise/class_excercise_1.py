47921dd2733d7c289c4400bcb439566fecfdf961
bhaskar <37036491+bhaskars9@users.noreply.github.com>
2022-09-13 19:42:46 -0400

    update

---
+++ class excercise/class_excercise_1.py
@@ -0,0 +1,13 @@
+num_1 = int(input("Enter 1st number "))
+num_2 = int(input("Enter 2nd number "))
+
+res_1 = num_1 + num_2
+print(res_1)
+res_2 = num_1-num_2
+print(res_2)
+res_3 = num_1*num_2
+print(res_3)
+res_4 = num_1**num_2
+print(res_4)
+res_5 = num_1/num_2
+print(res_5)
