f014334951f95107c44d36c5f0db38a0cb0842a4
BlueLobster413 <59360884+BlueLobster413@users.noreply.github.com>
2022-09-13 20:01:34 -0400

    Update class_excercise_1.py

--- class excercise/class_excercise_1.py
+++ class excercise/class_excercise_1.py
@@ -1,14 +1,20 @@
 num_1 = int(input("Enter 1st number "))
 num_2 = int(input("Enter 2nd number "))
+num3 = int(input('Enter 3rd number '))
 
-res_1 = num_1 + num_2
+res_1 = num_1 + num_2 + num3
 print("+",res_1)
-res_2 = num_1-num_2
-print("-",res_2)
-res_3 = num_1*num_2
-print("*",res_3)
-res_4 = num_1**num_2
-print("**",res_4)
-res_5 = num_1/num_2
-print("/",res_5)
 
+res_2 = num_1-num_2 - num3
+print("-",res_2)
+
+res_3 = num_1*num_2 * num3
+print("*",res_3)
+
+res_4 = num_1**num_2
+res_6 = res_4**num3
+print("**",res_6)
+
+res_5 = num_1/num_2
+res_7 = res_5/num3
+print("/",res_7)
